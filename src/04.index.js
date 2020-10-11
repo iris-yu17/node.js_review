cf12a020214fd89dd92d2b4c147a703edfe582ec
Iris Yu <iris.yu0716@hmail.com>
2020-10-11 16:16:15 +0800

    add

--- src/04.index.js
+++ src/04.index.js
@@ -1,14 +1,32 @@
+//--------require--------//
 // step1. require express套件
 const express = require('express')
 
+//--------建立app-------//
 // step2. 建立 web server 物件
 // 建立方法: 看api寫法，有時要用new，有時直接呼叫
 const app = express()
 
+
+//---------設定---------//
 // 設定樣版引擎
 // 放在require後(建立之後做設定)
 app.set('view engine', 'ejs');
 
+
+//---------top-level middleWare---------//
+// 用middleWare時都用use(use: http的甚麼方法都接受)
+// middleWare: 在中間處理，處裡完往下傳遞
+// top-level middleWare
+app.use(express.urlencoded({ extended: false }));
+// 原本 const parser = express.urlencoded({ extended: false });
+
+// top-level middleWare
+// 可放多個top-level middleWare，會自己判斷該用哪個
+app.use(express.json());
+
+
+//----------路由------------//
 // step3. 設定路由，類似http.createServer
 // get是html方法
 app.get('/', (req, res) => {
@@ -37,17 +55,38 @@ app.get('/json-sales', (req, res) => {
     res.render('json-sales', { sales })
 })
 
+// 取的queryString資料
+// queryString: get參數(url後面a=1&b=3)
+app.get('/try-qs', (req, res) => {
+    res.json(req.query);
+})
+
+// 取的post資料 & middleWare
+// 使用 express 物件的 body-parser 功能
+// urlencoded是body-parser裡面的功能
+// extended: true就使用qs lib, false使用內建的 querystring lib
+// 把parser當top-level middleware放最前面
+app.post('/try-post', (req, res) => {
+    // 把parser當middleWare，放在第二個參數
+    // app.post('/try-post', parser, (req, res) => {
+    res.json(req.body);
+})
+
+
+//----------靜態內容------------//
 // 使用靜態內容的資料夾，原則上放404頁前面
 // 靜態: 後端完全不會去改變到其內容
 // 動態: 後端生出來的(eg.404頁面)
 // 作法: 呼叫express的static方法
+// 這也是一個middleWare(在中間處理，處裡完往下傳遞)
 app.use(express.static(__dirname + '/../public'))
 
 
-
+//----------404頁面------------//
 // *** 此段放在所有路由設定的後面 ***
 // 因express框架是 first in first out
 // 自訂404頁面
+// 這不是middleWare，因沒有再往下傳遞)
 app.use((req, res) => {
     // type指的是contentType，詳見express doncment
     res.type('text/plain')
